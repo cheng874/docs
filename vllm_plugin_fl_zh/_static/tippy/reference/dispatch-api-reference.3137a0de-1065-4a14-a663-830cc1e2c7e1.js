c80928551317974897a8ebf0ce2814491ec9c15d
cheng874 <cheng874@users.noreply.github.com>
2026-09-22 02:31:38 +0000

    deploy: 60960e560e4cfa1227fc4f42cf531026a6f4cbde

---
+++ vllm_plugin_fl_zh/_static/tippy/reference/dispatch-api-reference.3137a0de-1065-4a14-a663-830cc1e2c7e1.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#id5\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">\u65e5\u5fd7<a class=\"headerlink\" href=\"#id5\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#id3\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">\u7ba1\u7406\u5668<a class=\"headerlink\" href=\"#id3\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#id2\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">\u7b56\u7565\u7ba1\u7406<a class=\"headerlink\" href=\"#id2\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#id1\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">\u4fbf\u6377\u51fd\u6570<a class=\"headerlink\" href=\"#id1\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#api\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">\u8c03\u5ea6 API \u53c2\u8003<a class=\"headerlink\" href=\"#api\" title=\"Link to this heading\">#</a></h1><h2>\u4fbf\u6377\u51fd\u6570<a class=\"headerlink\" href=\"#id1\" title=\"Link to this heading\">#</a></h2>", "a[href=\"#id4\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">\u63d2\u4ef6\u53d1\u73b0<a class=\"headerlink\" href=\"#id4\" title=\"Link to this heading\">#</a></h2>"}
+skip_classes = ["headerlink", "sd-stretched-link"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(` ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
